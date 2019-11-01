2af6942198ccc546e4197ec3a8d897d38b923527
gmallory <gmallory3@gmail.com>
2019-11-01 10:38:14 -0400

    Build out instructions for setting up and running database.

--- database/tables.py
+++ database/tables.py
@@ -1,20 +1,36 @@
-# Example: of ORM set up for one table
+"""
+Example of SQL Alchemy ORM set up for a US State table.
+
+"""
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from geoalchemy2 import Geometry
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/geospatial_usa_states'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+class States(db.Model):
+    __tablename__ = 'us_state'
+    gid = db.Column(db.Integer, primary_key=True)
+    region = db.Column(db.String)
+    division = db.Column(db.String)
+    statefp = db.Column(db.String)
+    statens = db.Column(db.String)
+    geoid = db.Column(db.String)
+    stusps = db.Column(db.String)
+    name = db.Column(db.String)
+    lsad = db.Column(db.String)
+    mtfcc = db.Column(db.String)
+    funcstat = db.Column(db.String)
+    aland = db.Column(db.Float)
+    awater = db.Column(db.Float)
+    intptlat = db.Column(db.String)
+    intptlon = db.Column(db.String)
+    geom = db.Column(Geometry('POLYGON'))
 
 
-db.session.add(User(name="Flask", email="example@example.com"))
-db.session.commit()
-
-users = User.query.all()
+state_records = States.query.all()
--- readme.md
+++ readme.md
@@ -1,6 +1,22 @@
+## US State Address Geolocator
 
-Setup:
-1. TBD
+###Code setup:
+1. Install the project requirements using `pip install -r requirements.txt`
+2. 
+
+
+###Database Setup (optional):
+1. Install the Postgres.app application (https://postgresapp.com/). 
+    * This gives us an easy-to-set up Postgres database, the PostGIS extension, a tool to import shape files into a PostGIS-enabled database called shp2pgsql
+2. Open Postgres.app to start a Postgres server with 3 default databases.
+3. Using a tool such as pgAdmin, add the extension PostGIS (see image below)
+4. Now it is possible to execute the command to load the shape files. Run the following in your command line, substituting values where necessary.
+    * `shp2pgsql resources/shape_files/tl_2019_us_state.shp DATATABLE | psql -U DATABASE_USER -d DATABASE_NAME`
+5. Test by running a select statement against the table!
+    * Note, you'll either need to double click your database in Postgress.app or run `psql -h localhost -U DATABASE_USER DATABASE_NAME` to start a db shell.
+    
+<img src="https://github.com/gmallory3/US-State-Geocode/tree/master/resources/DB_setup_img_001.png" width="48">
+
 
 Run code by:
 1. TBD
